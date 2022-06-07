3533608ac3839f99ef28754632dc314f23bddb7b
WQuainoo <wquainoo77@gmail.com>
2022-06-07 18:36:28 -0400

    Water page styled and put together

--- NatureSlider.js
+++ NatureSlider.js
@@ -1,8 +1,17 @@
-let earthSlide = document.querySelector('.earth');
+let earthSlide = document.querySelector('.earth')
 let mountain = document.querySelector('.stability')
+
+let waterSlide = document.querySelector('.water')
+let river = document.querySelector('.flow')
 
 earthSlide.onclick = function()
 {
     earthSlide.classList.toggle('active');
     mountain.classList.toggle('active')
+}
+
+waterSlide.onclick = function()
+{
+    waterSlide.classList.toggle('active');
+    river.classList.toggle('active')
 }
